8d193ac69057aea6ba7e97de0af8c26a0da4e7f0
Marjorie Tungcul <marjorietungcul11@gmail.com>
2025-10-28 09:08:32 +0800

    initial

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'beacon-pwa-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/calendar.html',
+  '/map.html',
+  '/report.html',
+  '/notification.html',
+  '/login.html',
+  '/register.html',
+  '/information.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
